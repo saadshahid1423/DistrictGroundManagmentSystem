1848fc974cffc85eca39c68c7921dac1333557ed
saadshahid1423 <saadshahid1423@gmail.com>
2024-01-06 00:14:37 +0500

    GMS first push

---
+++ DistrictPlayGroundManagementSystem/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace DistrictPlayGroundManagementSystem
+{
+    public abstract class Class1
+    {
+    }
+}
